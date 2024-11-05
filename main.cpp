7d0bde7c0d7c2396f4ae1a1cc85cd79aff203e34
Daniel <danieloftavares@gmail.com>
2024-11-05 10:51:23 -0300

    Added screen and shapes using raylib

--- main.cpp
+++ main.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
-
+#include <raylib.h>
 
 int main(){
+	
+	std::cout << "Initializing programm" << std::endl;
+	const int width = 1000;
+	const int height = 800;
+	const char image1[] = "/home/daniel/Desktop/path/photo1.png";
+	const char image2[] = "/home/daniel/Desktop/path/photo2.png";
+	const char image3[] = "/home/daniel/Desktop/path/photo3.png";
+	const char image4[] = "/home/daniel/Desktop/path/photo4.png";
+	const char image5[] = "/home/daniel/Desktop/path/photo5.png";
+	const char image6[] = "/home/daniel/Desktop/path/photo6.png";
 
-	std::cout << "Creating and configuring the repo";
-        return 0;
+
+	Rectangle baseScreen = {50, 50, width-100, height-100};
+
+	InitWindow(width, height, "YARA");
+	SetTargetFPS(60);
+
+	Texture2D background = LoadTexture(image1);
+
+	while(!WindowShouldClose()){
+		ShowCursor();
+		BeginDrawing();
+		//DrawTexture(background, 0, 0, WHITE);
+		DrawRectangleRounded(baseScreen, 0.1, 10, LIGHTGRAY);
+		EndDrawing();
+
+	}
+	UnloadTexture(background);
+
+	CloseWindow();
+	return 0;
 
 }
-
