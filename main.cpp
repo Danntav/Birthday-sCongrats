ad370abde51f3d61b2aa368deaf70090a6a8080c
Daniel <danieloftavares@gmail.com>
2024-11-08 13:56:26 -0300

    Scaled images, hover effect buttons, background slide image, more pages

--- main.cpp
+++ main.cpp
@@ -4,17 +4,18 @@
 const int width = 1000;
 const int height = 800;
 
-void Screens(int currentPage, Texture2D images[]);
+void Screens(int currentPage, Texture2D images[], Texture2D background);
 
 int main(){
 	
 	std::cout << "Initializing programm" << std::endl;
 
-	const char backgroundMenu[] = "/home/daniel/Desktop/path/background.png";
+	const char backgroundMenu[] = "/home/daniel/Desktop/path/backgroundMenu.png";
+	const char backgroundSlide[] = "/home/daniel/Desktop/path/backgroundSlide.png";
 
 	bool startButtonState = false;
 	int currentPage = 1;
-	int numberOfPages = 5;
+	int numberOfPages = 10;
 
 	Rectangle startButton = {(width/2)-75, height-300,150 ,50 };
 	Rectangle nextButton = {width-200,height-100,150,50};
@@ -25,26 +26,26 @@ int main(){
 	InitWindow(width, height, "YARA");
 	SetTargetFPS(60);
 	Texture2D backgroundMenuImage = LoadTexture(backgroundMenu);
-
+	Texture2D backgroundSlideImage = LoadTexture(backgroundSlide);
+	
 	Texture2D images[numberOfPages] = {
 		LoadTexture("/home/daniel/Desktop/path/photo1.png"),
 		LoadTexture("/home/daniel/Desktop/path/photo2.png"),
-		LoadTexture("/home/daniel/Desktop/path/photo3.png"),
+		LoadTexture("/home/daniel/Desktop/path/photo10.png"),
 		LoadTexture("/home/daniel/Desktop/path/photo4.png"),
 		LoadTexture("/home/daniel/Desktop/path/photo5.png"),
-		LoadTexture("/home/daniel/Desktop/path/photo6.png"),
+		LoadTexture("/home/daniel/Desktop/path/photo11.png"),
 		LoadTexture("/home/daniel/Desktop/path/photo7.png"),
 		LoadTexture("/home/daniel/Desktop/path/photo8.png"),
 		LoadTexture("/home/daniel/Desktop/path/photo9.png"),
 	};
-
 
 	while(!WindowShouldClose()){
 		ShowCursor();
 		BeginDrawing();
 		 if (startButtonState){
 			ClearBackground(WHITE);
-		 	Screens(currentPage-1, images);
+		 	Screens(currentPage-1, images, backgroundSlideImage);
 		        DrawRectangleRounded(nextButton, 0.3, 0, LIGHTGRAY);
 			DrawRectangleRounded(previousButton, 0.3, 0, LIGHTGRAY);
 			DrawRectangleRounded(menuButton, 0.3, 0, RED);
@@ -52,16 +53,21 @@ int main(){
 			DrawText(" -> ", width-175, height-100, 60, BLACK);
 			DrawText(" <- ", 75, height-100, 60, BLACK);
 
-		        if (CheckCollisionPointRec(GetMousePosition(), nextButton) && IsMouseButtonPressed(0)){
-		                currentPage++;
-				if (currentPage > numberOfPages){
-					currentPage = 1;
-				}
+		        if (CheckCollisionPointRec(GetMousePosition(), nextButton)){
+				DrawRectangleRounded(nextButton, 0.3, 0, {190,190,190,200});
+                                if (IsMouseButtonPressed(0)){
+					currentPage++;
+					if (currentPage > numberOfPages){
+						currentPage = 1;
+				}	}
        			 }
-			if (CheckCollisionPointRec(GetMousePosition(), previousButton) && IsMouseButtonPressed(0)){
-				currentPage--;
-				if(currentPage < 1){
-					currentPage = numberOfPages;
+			if (CheckCollisionPointRec(GetMousePosition(), previousButton)){
+				DrawRectangleRounded(previousButton, 0.3, 0, {190,190,190,200});
+				if (IsMouseButtonPressed(0)){
+					currentPage--;
+					if(currentPage < 1){
+						currentPage = numberOfPages;
+					}
 				}
 			}
 			if (CheckCollisionPointRec(GetMousePosition(), menuButton) && IsMouseButtonPressed(0)){
@@ -102,12 +108,23 @@ int main(){
 }
 
 
-void Screens(int currentPage, Texture2D image[]){
+void Screens(int currentPage, Texture2D image[], Texture2D background){
+	
+	float scaleX = static_cast<float>(width) / image[currentPage].width;
+	float scaleY = static_cast<float>(height) / image[currentPage].height;
+	float scale = std::min(scaleX, scaleY); // Uniform scale to keep aspect ratio
 
-	DrawTexture(image[currentPage], 0, 0, WHITE);
+	float posX = (width - image[currentPage].width * scale) / 2;
+	float posY = (height - image[currentPage].height * scale) / 2;
 
+
+	DrawTextureEx(background,{0,0}, 0.0f,0.7 ,WHITE);
+
+	DrawTextureEx(image[currentPage], {posX, posY}, 0.0f, scale, WHITE);
 
 	switch(currentPage){
+		case 0:
+			break;
 		case 1:
 			DrawText("Welcome to Page 1", 50, 50, 20, BLACK);
 			break;
@@ -121,11 +138,22 @@ void Screens(int currentPage, Texture2D image[]){
 			DrawText("Forth Image", 50, 50, 20, BLACK);
 			break;
 		case 5:
+			DrawText("Fifth Image", 50, 50, 20, BLACK);
 			break;
 		case 6:
+			DrawText("Sixth Image", 50, 50, 20, BLACK);
 			break;
-
-
+		case 7:
+			DrawText("Seventh Image", 50, 50, 20, BLACK);
+                        break;
+		case 8:
+			DrawText("Eigth Image", 50, 50, 20, BLACK);
+                        break;
+		case 9:
+			DrawText("Nineth Image", 50, 50, 20, BLACK);
+                        break;
+		case 10:
+			DrawText("Tenth Image", 50, 50, 20, BLACK);
+			break;
 	}
-
 }
