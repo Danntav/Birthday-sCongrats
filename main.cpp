b38b53aae0f4dc2abcac1812460237d1aae6fb37
Daniel <danieloftavares@gmail.com>
2025-04-21 18:42:38 -0300

    Generic Text

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ int main(){
 	Rectangle exitButton = {(width/2)-75, height-240,150 ,50 };
 	
 	//Initializing program window and loading images
-	InitWindow(width, height, "YARA");
+	InitWindow(width, height, "{NAME}");
 	SetTargetFPS(60);
 	Texture2D backgroundMenuImage = LoadTexture(backgroundMenu);
 	Texture2D backgroundSlideImage = LoadTexture(backgroundSlide);
@@ -36,11 +36,11 @@ int main(){
 	Texture2D images[numberOfPages] = {
 		LoadTexture("photo1.png"),
 		LoadTexture("photo2.png"),
-		LoadTexture("photo10.png"),
+		LoadTexture("photo3.png"),
 		LoadTexture("photo4.png"),
 		LoadTexture("photo5.png"),
-		LoadTexture("photo12.png"),
 		LoadTexture("photo6.png"),
+		LoadTexture("photo7.png"),
 		LoadTexture("photo8.png"),
 		LoadTexture("photo9.png"),
 	};
@@ -87,12 +87,12 @@ int main(){
 			ClearBackground(WHITE);
 			DrawTexture(backgroundMenuImage, 0, 0, WHITE);
 			DrawRectangleRounded({(width/2)-200, height-525,400 ,200 }, 0.3, 0, WHITE);
-			DrawText("Feliz Aniversario", (width/2)-175, height-500, 42, BLACK);
-			DrawText("  MEU AMOR!   ", (width/2)-175, height-420, 50, BLACK);
+			DrawText("Happy Birthday!", (width/2)-175, height-500, 42, BLACK);
+			DrawText("  {Text}   ", (width/2)-175, height-420, 50, BLACK);
 	                DrawRectangleRounded(startButton, 0.3, 0, LIGHTGRAY);
-        	        DrawText("COMECAR", (width/2)-50, height-285, 20, BLACK);
+        	        DrawText("START", (width/2)-50, height-285, 20, BLACK);
 			DrawRectangleRounded(exitButton, 0.3, 0,RED);
-            	        DrawText("SAIR", (width/2)-25, height-225, 20, BLACK);
+            	        DrawText("EXIT", (width/2)-25, height-225, 20, BLACK);
 			
 			//Checking menu buttons
 			if(CheckCollisionPointRec(GetMousePosition(), startButton)){
@@ -138,40 +138,40 @@ void Screens(int currentPage, Texture2D image[], Texture2D background, Texture2D
 	switch(currentPage){
 		case 0:
 			DrawRectangleRounded({200, height-120, 600, 90}, 0.3, 0, WHITE);
-			DrawText("Dedico esta mensagem para o amor da minha vida", 220, height-80, 23, BLACK);
+			DrawText("{Text}", 220, height-80, 23, BLACK);
 			break;
 		case 1:
 			DrawRectangleRounded({200, 50, 600, 90}, 0.3, 0, WHITE);
-			DrawText("Esta mulher companheira que está sempre comigo", 215, 80, 23, BLACK);
+			DrawText("{Text}", 215, 80, 23, BLACK);
 			break;
 		case 2:
 			DrawRectangleRounded({200, 50, 600, 90}, 0.3, 0, WHITE);
-			DrawText("Que me ensina a ser um homem melhor a cada dia", 215, 80, 23, BLACK);
+			DrawText("{Text}", 215, 80, 23, BLACK);
 			break;
 		case 3:
 			DrawRectangleRounded({200, height-120, 600, 90}, 0.3, 0, WHITE);
-	        DrawText("Esta mulher incrível, inteligiente, linda... ", 260, height-80, 23, BLACK);
+	        DrawText("{Text}", 260, height-80, 23, BLACK);
 	        break;
 		case 4:
 			DrawRectangleRounded({200, height-120, 600, 90}, 0.3, 0, WHITE);
-			DrawText("PERFEITA!", (width/2)-110, height-85, 40, BLACK);
+			DrawText("{Text}", (width/2)-110, height-85, 40, BLACK);
 			break;
 		case 5:
 			DrawRectangleRounded({200, 50, 600, 90}, 0.3, 0, WHITE);
-			DrawText("A melhor companhia de todos os momentos", 250, 80, 23, BLACK);
+			DrawText("{Text}", 250, 80, 23, BLACK);
 			break;
 		case 6:
 			DrawRectangleRounded({200, 50, 600, 90}, 0.3, 0, WHITE);
-			DrawText("Que transforma momentos simples", 290, 70, 23, BLACK);
-			DrawText("em memórias inesquecíveis", 310, 95, 23, BLACK);
+			DrawText("{Text}", 290, 70, 23, BLACK);
+			DrawText("{Text}", 310, 95, 23, BLACK);
 			break;
 		case 7:
 			DrawRectangleRounded({200, height-120, 600, 90}, 0.3, 0, WHITE);
-			DrawText("Agradeço por cada sorriso e por cada abraço", 225, height-80, 23, BLACK);
+			DrawText("{Text}", 225, height-80, 23, BLACK);
                         break;
 		case 8:
 			DrawRectangleRounded({200, height-120, 600, 90}, 0.3, 0, WHITE);
-			DrawText("Te amo eternamente", (width/2)-110, height-80, 23, BLACK);
+			DrawText("{Text}", (width/2)-110, height-80, 23, BLACK);
                         break;
 		case 9:
 			//Set small animated button on the last page
@@ -180,12 +180,12 @@ void Screens(int currentPage, Texture2D image[], Texture2D background, Texture2D
 		        static Rectangle dontLoveButton = {width/2-100, height/2+160, 200, 50};
 			
 		        DrawRectangleRounded(loveButton, 0.3, 0, LIGHTGRAY);
-		        DrawText("    Te amo", loveButton.x + 30, loveButton.y + 15, 20, BLACK);
+		        DrawText("    {Text}", loveButton.x + 30, loveButton.y + 15, 20, BLACK);
 		        DrawRectangleRounded(dontLoveButton, 0.3, 0, LIGHTGRAY);
-		        DrawText("   Nao te amo", dontLoveButton.x + 20, dontLoveButton.y + 15, 20, BLACK);
+		        DrawText("   {Text}", dontLoveButton.x + 20, dontLoveButton.y + 15, 20, BLACK);
 		        if (CheckCollisionPointRec(GetMousePosition(), loveButton)){
 				DrawRectangleRounded(loveButton,0.3,0, {190,190,190,200});
-				DrawText("Te amo mais, amor", 250, 200, 60, BLACK);
+				DrawText("{Text}", 250, 200, 60, BLACK);
 				DrawTextureEx(heart,{425,300} , 0.0f, 0.1, WHITE);
 			}
 			if (CheckCollisionPointRec(GetMousePosition(), dontLoveButton)) {
